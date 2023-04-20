d0e4bff7243cc00596eb853aa2f8c1f367633f9e
Edvins Mineikis <edvinsmineikis@gmail.com>
2023-04-20 11:42:05 +0300

    dockerize project

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import json
+import os
 
 app = Flask('sensorApp')
 app.config['JSON_AS_ASCII'] = False
@@ -11,19 +12,19 @@ api = Api(app)
 
 class Metrics(Resource):
     def get(self):
-        with open('api/data/metrics.json', encoding='utf-8') as file:
+        with open('data/metrics.json', encoding='utf-8') as file:
             json_data = json.load(file)
         return json_data, 200
 
 class Sensors(Resource):
     def get(self):
-        with open('api/data/sensors.json', encoding='utf-8') as file:
+        with open('data/sensors.json', encoding='utf-8') as file:
             json_data = json.load(file)
         return json_data, 200
 
 class SensorTypes(Resource):
     def get(self):
-        with open('api/data/sensorTypes.json', encoding='utf-8') as file:
+        with open('data/sensorTypes.json', encoding='utf-8') as file:
             json_data = json.load(file)
         return json_data, 200
 
@@ -32,4 +33,5 @@ api.add_resource(Sensors, '/sensors.json')
 api.add_resource(SensorTypes, '/sensorTypes.json')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
